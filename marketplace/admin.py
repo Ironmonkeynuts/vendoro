15ffd5aba0afd78b7ee1444402734e1c0cd5c5ed
David Doyle-Owen <thinkimagine@outlook.com>
2025-08-29 20:36:37 +0100

    Add Category and Product models and resgister to admin

--- marketplace/admin.py
+++ marketplace/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Shop
+from .models import Shop, Category, Product
 
 
 admin.site.register(Shop)
+admin.site.register(Category)
+admin.site.register(Product)
--- marketplace/models.py
+++ marketplace/models.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.core.validators import MinValueValidator
 from django.db import models
 from django.utils.text import slugify
 
@@ -7,9 +8,11 @@ User = settings.AUTH_USER_MODEL
 
 
 class Shop(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='shops')
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='shops'
+    )
     name = models.CharField(max_length=255)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, blank=True)
     tagline = models.CharField(max_length=255, blank=True)
     primary_color = models.CharField(max_length=7, default='#111827')
     highlight_color = models.CharField(max_length=7, default='#06b6d4')
@@ -32,3 +35,45 @@ class Shop(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    shop = models.ForeignKey(
+        Shop, on_delete=models.CASCADE, related_name='products'
+    )
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(blank=True)
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2, validators=[MinValueValidator(0)]
+    )
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ("shop", "title")
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.title} ({self.shop.name})"
